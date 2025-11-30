8125d83c2eaf45bc5533421aa5154d68eda36ef0
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-11-30 18:11:07 +0100

    first simple view component demo

---
+++ src/main/java/org/svenehrke/demo/web/simplevcdemos/ApplicationPageComponent.java
@@ -0,0 +1,23 @@
+package org.svenehrke.demo.web.simplevcdemos;
+
+
+import de.tschuehly.spring.viewcomponent.core.component.ViewComponent;
+import de.tschuehly.spring.viewcomponent.jte.ViewContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@ViewComponent
+@Controller
+public class ApplicationPageComponent {
+
+	public static final String URL = "/ui/demos/simplevc/applicationpage";
+
+	public record Ctx() implements ViewContext {
+	}
+
+	@GetMapping(URL)
+	public Ctx ctx(
+	) {
+		return new Ctx();
+	}
+}
