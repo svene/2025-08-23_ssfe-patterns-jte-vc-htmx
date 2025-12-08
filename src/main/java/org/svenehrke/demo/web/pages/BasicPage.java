53412e50bedcda57bc1b4b278214f8b60c1cee2f
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-08 19:36:30 +0100

    page patterns: content page with and without param

---
+++ src/main/java/org/svenehrke/demo/web/pages/BasicPage.java
@@ -0,0 +1,20 @@
+package org.svenehrke.demo.web.pages;
+
+import de.tschuehly.spring.viewcomponent.core.component.ViewComponent;
+import de.tschuehly.spring.viewcomponent.jte.ViewContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@ViewComponent
+@Controller
+public class BasicPage {
+	public static final String URL = "/ui/pages/basicpage";
+
+	public record Ctx() implements ViewContext {
+	}
+
+	@GetMapping(URL)
+	public Ctx ctx() {
+		return new Ctx();
+	}
+}
