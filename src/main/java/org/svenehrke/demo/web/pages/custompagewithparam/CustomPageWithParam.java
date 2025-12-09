fbeb140554b3525f8add056bbddaf90756ff5ae9
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-09 05:24:04 +0100

    page patterns: CustomPageWithParam: refactoring

--- src/main/java/org/svenehrke/demo/web/pages/custompagewithparam/CustomPageWithParam.java
+++ src/main/java/org/svenehrke/demo/web/pages/custompagewithparam/CustomPageWithParam.java
@@ -1,4 +1,4 @@
-package org.svenehrke.demo.web.pages;
+package org.svenehrke.demo.web.pages.custompagewithparam;
 
 import de.tschuehly.spring.viewcomponent.core.component.ViewComponent;
 import de.tschuehly.spring.viewcomponent.jte.ViewContext;
