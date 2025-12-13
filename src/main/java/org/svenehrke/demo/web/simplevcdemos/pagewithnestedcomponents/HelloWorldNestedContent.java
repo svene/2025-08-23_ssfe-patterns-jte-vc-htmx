c5fe7ce3efbc8e88d9bcb0e6f2cca2ee62b613c3
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-13 11:54:08 +0100

    VC demos: PageWithNestedComponents

---
+++ src/main/java/org/svenehrke/demo/web/simplevcdemos/pagewithnestedcomponents/HelloWorldNestedContent.java
@@ -0,0 +1,17 @@
+package org.svenehrke.demo.web.simplevcdemos.pagewithnestedcomponents;
+
+
+import de.tschuehly.spring.viewcomponent.core.component.ViewComponent;
+import de.tschuehly.spring.viewcomponent.jte.ViewContext;
+import gg.jte.Content;
+
+@ViewComponent
+public class HelloWorldNestedContent {
+
+	public record Ctx(Content greetingText) implements ViewContext {
+	}
+
+	public static Ctx ctx(Content greetingText) {
+		return new Ctx(greetingText);
+	}
+}
---
+++ src/main/java/org/svenehrke/demo/web/simplevcdemos/pagewithnestedcomponents/PageWithNestedComponents.java
@@ -0,0 +1,19 @@
+package org.svenehrke.demo.web.simplevcdemos.pagewithnestedcomponents;
+
+import de.tschuehly.spring.viewcomponent.core.component.ViewComponent;
+import de.tschuehly.spring.viewcomponent.jte.ViewContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@ViewComponent
+@Controller
+public class PageWithNestedComponents {
+	public static final String URL = "/ui/demos/simplevc/pagewithnestedcomponents";
+	public record Ctx() implements ViewContext {
+	}
+	@GetMapping(URL)
+	public Ctx ctx(
+	) {
+		return new Ctx();
+	}
+}
