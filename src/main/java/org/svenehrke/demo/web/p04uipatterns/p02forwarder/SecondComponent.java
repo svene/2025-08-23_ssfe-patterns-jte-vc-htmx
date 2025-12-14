2628703a5c8d38903c65f43c0cb5e449fbabceaf
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-14 20:01:30 +0100

    forwarder example

--- src/main/java/org/svenehrke/demo/web/p04uipatterns/p02forwarder/SecondComponent.java
+++ src/main/java/org/svenehrke/demo/web/p04uipatterns/p02forwarder/SecondComponent.java
@@ -1,4 +1,4 @@
-package org.svenehrke.demo.web.components.componentforwarder;
+package org.svenehrke.demo.web.p04uipatterns.p02forwarder;
 
 
 import de.tschuehly.spring.viewcomponent.core.component.ViewComponent;
