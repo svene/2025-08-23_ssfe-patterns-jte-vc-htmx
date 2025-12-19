63506043eaf13af8d37fe338356998b746066661
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-19 09:55:13 +0100

    code explanations: plain JTE 04

---
+++ src/main/java/jte/plainjte/Page04PassContentCode.java
@@ -0,0 +1,34 @@
+package jte.plainjte;
+
+public class Page04PassContentCode {
+	public static String PAGE_JTE = """
+      <h1>Application Page</h1>
+
+      @template.jte.components.helloworldcontent(
+        greetingText = @` <%-- include HTML content --%>
+          How are you?<br>
+          Hopefully <strong>very well</strong>.<br>
+          <p>See you soon!</p>
+        `
+      )
+      """;
+
+	public static String HELLO_WORLD_JTE = """
+      @param gg.jte.Content greetingText = @``
+
+      ${greetingText}
+      """;
+
+	public static String PAGE_JAVA = """
+      @Controller
+      public class PlainJTEController {
+        public static final String PAGE_04_PASS_CONTENT_URL = "/page04passcontent";
+
+        @GetMapping(PAGE_04_PASS_CONTENT_URL)
+        public String appPagePassContent() {
+          return "jte/plainjte/page04passcontent";
+        }
+      }
+      """;
+
+}
