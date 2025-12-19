f226814e12723c0a97987b741cc219bd29b7eaa2
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-19 07:11:55 +0100

    code explanations: plain JTE 02

---
+++ src/main/java/jte/plainjte/Page02InclCode.java
@@ -0,0 +1,27 @@
+package jte.plainjte;
+
+public class Page02InclCode {
+	public static String PAGE_JTE = """
+      <h1>Application Page</h1>
+
+      <!-- Include Component: -->
+      @template.jte.components.helloworld()
+      """;
+
+	public static String PAGE_JAVA = """
+      @Controller
+      public class PlainJTEController {
+        public static final String PAGE_02_INCL_URL = "/page02incl";
+
+        @GetMapping(PAGE_02_INCL_URL)
+        public String app() {
+          return "jte/plainjte/page02incl";
+        }
+      }
+      """;
+
+	public static String HELLO_WORLD_JTE = """
+      <h3>Hello world!</h3>
+      """;
+
+}
