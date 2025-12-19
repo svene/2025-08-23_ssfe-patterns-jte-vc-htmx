b22d2abefd09e29211b2ef59ddf236d18cf89490
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-19 06:26:30 +0100

    code explanations: codesnippet.jte for ContentPageWithParam

--- src/main/java/org/svenehrke/demo/web/p03pages/p02contentpagewithparam/ContentPageWithParamCode.java
+++ src/main/java/org/svenehrke/demo/web/p03pages/p02contentpagewithparam/ContentPageWithParamCode.java
@@ -7,5 +7,29 @@ import org.springframework.stereotype.Controller;
 @ViewComponent
 @Controller
 public class ContentPageWithParamCode {
-	public record Ctx() implements ViewContext { }
+	public record Ctx() implements ViewContext {}
+
+	public static String PAGE_JTE = """
+      @template.jte.pages.bulmapage(
+        content = @`
+          <h1>Content Page with Param says: ${ctx.greeting()}</h1>
+      `)
+      """;
+
+	public static String PAGE_JAVA = """
+      @ViewComponent
+      @Controller
+      public class ContentPageWithParam {
+        public static final String URL = "/ui/pages/contentpagewithparam";
+        public record Ctx(String greeting) implements ViewContext {}
+
+        @GetMapping(URL)
+        public Ctx ctx(
+          @RequestParam(name = "greeting", required = false, defaultValue = "Hello")
+          String greeting
+        ) {
+          return new Ctx(greeting);
+        }
+      }
+      """;
 }
