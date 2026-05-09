96fb9b2e0a4d80422a7bd5fffc62ba3f9a61761b
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-05-09 18:37:17 +0200

    s02p04: extracted the documentation

--- src/main/java/org/svenehrke/demo/web/p02simplevcdemos/page04withcontentparamscomponent/HelloWorldContent.java
+++ src/main/java/org/svenehrke/demo/web/p02simplevcdemos/page04withcontentparamscomponent/HelloWorldContent.java
@@ -5,7 +5,9 @@ import de.tschuehly.spring.viewcomponent.core.component.ViewComponent;
 import de.tschuehly.spring.viewcomponent.jte.ViewContext;
 import gg.jte.Content;
 
+// docs:start component
 @ViewComponent
 public class HelloWorldContent {
 	public record Ctx(Content greetingText) implements ViewContext {}
 }
+// docs:end component
--- src/main/java/org/svenehrke/demo/web/p02simplevcdemos/page04withcontentparamscomponent/PageWithContentParamsComponent.java
+++ src/main/java/org/svenehrke/demo/web/p02simplevcdemos/page04withcontentparamscomponent/PageWithContentParamsComponent.java
@@ -5,6 +5,7 @@ import de.tschuehly.spring.viewcomponent.jte.ViewContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// docs:start page
 @ViewComponent
 @Controller
 public class PageWithContentParamsComponent {
@@ -15,3 +16,4 @@ public class PageWithContentParamsComponent {
 		return new Ctx();
 	}
 }
+// docs:end page
